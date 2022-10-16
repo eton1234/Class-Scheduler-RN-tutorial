0188054e4a172d1fc38716fd4a4537f85603f06a
Ethan Chan <eton1234@outlook.com>
2022-10-16 14:55:13 -0500

    initial commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Welcome to my app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00f',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, textStyle: {
+    color: '#fff',
+    fontSize: 32, 
+  }
 });
